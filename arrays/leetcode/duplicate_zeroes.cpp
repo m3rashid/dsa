35a22b27f1a24a9f31e6bd909b3d18c8333bcc01
m3rashid <mdrashid.hussain.786.01@gmail.com>
2022-07-27 15:17:39 +0530

    check

---
+++ arrays/leetcode/duplicate_zeroes.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void duplicateZeros(vector<int>& arr) {
+    for (auto i = arr.begin(); i < arr.end(); i++) {
+        if (*i == 0) {
+            arr.insert(i + 1, 0);
+        }
+    }
+}
+
+int main() {
+    vector<int> v;
+    int b;
+    cin >> b;
+    while (cin) {
+        v.push_back(b);
+        cin >> b;
+    }
+    duplicateZeros(v);
+    for (auto x : v) cout << x << " ";
+
+    return 0;
+}
