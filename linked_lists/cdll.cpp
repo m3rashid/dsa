fbb30ebe1a3564a42f49d25a1bded26b490a2018
MD Rashid Hussain <md2000399@st.jmi.ac.in>
2022-09-09 23:32:16 +0530

    about page

---
+++ linked_lists/cdll.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+   private:
+    int data;
+    ListNode* next;
+    ListNode* prev;
+
+   public:
+    ListNode(int d = 0) {
+        this->data = d;
+        this->next = this;
+        this->prev = this;
+    }
+
+    ListNode* getNode() {
+        return this;
+    }
+
+    int getData() {
+        return this->data;
+    }
+
+    ListNode* getThisNext() {
+        return this->next;
+    }
+
+    ListNode* getThisPrev() {
+        return this->prev;
+    }
+
+    void setThisNext(ListNode* n) {
+        this->next = n;
+    }
+
+    void setThisPrev(ListNode* n) {
+        this->prev = n;
+    }
+};
+
+class LinkedList {
+   private:
+    ListNode* head;
+    ListNode* previous;
+    ListNode* current;
+
+   public:
+    LinkedList() {
+        this->head = NULL;
+        this->current = NULL;
+        this->previous = NULL;
+    };
+
+    void addNodeAtTheEnd(int n = 0) {
+        ListNode* node = new ListNode(n);
+        this->previous = this->current;
+        this->current = node;
+
+        if (!(this->head)) {
+            this->head = node;
+        } else {
+            this->previous->setThisNext(node);
+            this->current->setThisPrev(this->previous);
+        }
+    }
+
+    ListNode* getHead() {
+        return this->head;
+    }
+
+    ListNode* getLast() {
+        return this->current;
+    }
+
+    void printListFromFront() {
+        ListNode* current = this->getHead();
+        while (current) {
+            cout << current->getData() << "  ";
+            current = current->getThisNext();
+            if (current = this->getHead()) return;
+        }
+        cout << endl;
+    }
+
+    void printListFromBack() {
+        ListNode* current = this->getLast();
+        while (current) {
+            cout << current->getData() << "  ";
+            current = current->getThisPrev();
+            if (current = this->getLast()) return;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    int arr[] = {5, 52, 33, 44, 15, 52, 312, 3123, 12};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    LinkedList ll = LinkedList();
+    for (int i = 0; i < n; i++) ll.addNodeAtTheEnd(arr[i]);
+
+    ll.getLast()->setThisNext(ll.getHead());
+    ll.getHead()->setThisPrev(ll.getLast());
+
+    ll.printListFromFront();
+    ll.printListFromBack();
+
+    return 0;
+}
---
+++ trees/main.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+   public:
+    int dataInt;
+    Node *leftNode;
+    Node *rightNode;
+
+    Node(int data = 0, Node *left = NULL, Node *right = NULL) {
+        dataInt = data;
+        leftNode = left;
+        rightNode = right;
+    }
+};
+
+class BinaryTree {
+   public:
+    Node *rootNode;
+
+    BinaryTree() {
+        Node *n = new Node();
+        rootNode = n;
+    }
+
+    BinaryTree(Node *root) {
+        rootNode = root;
+    }
+
+    void arrayToBinaryTree(vector<int> arr, int n, Node *root) {
+        // for(int i=1; i<=n; i++){
+        //     Node *n = new Node(arr[i]);
+        // }
+        static int i = 1;
+
+        root->leftNode = new Node(arr[(2 * i)]);
+        root->rightNode = new Node(arr[(2 * i) + 1]);
+
+        vector<int> forLeft;
+        vector<int> forRight;
+
+        // arrayToBinaryTree(copy(arr.begin(), arr.begin(), forLeft.begin()), n, root->leftNode);
+
+        // arrayToBinaryTree(copy(arr.begin(), arr.begin(), forLeft.begin()), n, root->rightNode);
+    }
+};
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 54, 34};
+    vector<int> result = {};
+
+    for (auto x : arr) cout << x << " ";
+    cout << endl;
+    cout << endl;
+
+    auto starting = arr.begin() + 1;
+    auto ending = arr.begin() + 3;
+
+    copy(starting, ending, result.begin());
+
+    for (auto x : arr) cout << x << " ";
+    cout << endl;
+    for (auto y : result) cout << y << " ";
+    cout << endl;
+
+    return 0;
+}
